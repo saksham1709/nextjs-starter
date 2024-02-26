6307785aa5bbe42d1944b4829fee63daefb56740
Disha <sakshamsharma1709@gmail.com>
2024-02-26 17:25:20 +0530

    navLinks added

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import styles from './links.module.css';
+import NavLink from "./navLink/NavLink";
 
 const Navbar = () => {
 
@@ -22,10 +24,11 @@ const Navbar = () => {
     ]
 
     return (
-        <div>
+        <div className={styles.container} >
             {
                 links.map(link => (
-                    <Link href={link.path} key={link.title} >{link.title}</Link>
+                    <NavLink item={link} key={link.title} />
+                    // <Link href={link.path} key={link.title} >{link.title}</Link>
                 ))
             }
         </div>

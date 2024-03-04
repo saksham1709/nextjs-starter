0ad6860ad32c561786d4412ef019c143bd060779
Disha <sakshamsharma1709@gmail.com>
2024-03-04 17:51:29 +0530

    UI completed

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,31 @@
-const SlugBlog = () => {
-    return <div>SlugBlog Page</div>;
+import Image from "next/image";
+import styles from "./singlePost.module.css"
+
+const SinglePost = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="https://images.pexels.com/photos/618833/pexels-photo-618833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.detail}>
+                    <Image src="https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.avatar} width={50} height={50} />
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Author</span>
+                        <span className={styles.detailValue}>John Doe</span>
+                    </div>
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Date</span>
+                        <span className={styles.detailValue}>17/09/2001</span>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum dolores reprehenderit deserunt, laboriosam culpa corrupti maiores quam quos ea nemo.
+                </div>
+            </div>
+        </div >
+    );
 };
 
-export default SlugBlog;
+export default SinglePost;
